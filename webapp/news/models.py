e05c72db42d3ac709b5b8454b0f3cdf2aa251149
fisheract <geremy85@gmail.com>
2020-07-12 18:40:18 +0600

    add comment section

--- webapp/news/models.py
+++ webapp/news/models.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+from sqlalchemy.orm import relationship
+
 from webapp.db import db
 
 
@@ -8,5 +11,30 @@ class News(db.Model):
     published = db.Column(db.DateTime, nullable=False)
     text = db.Column(db.Text, nullable=True)
 
+    def comments_count(self):
+        return Comments.query.filter(Comments.news_id == self.id).count()
+
     def __repr__(self):
         return '<News {} {}>'.format(self.title, self.url)
+
+
+class Comments(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Text, nullable=True)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.now())
+    news_id = db.Column(
+        db.Integer,
+        db.ForeignKey('news.id', ondelete='CASCADE'),
+        index=True
+    )
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('user.id', ondelete='CASCADE'),
+        index=True
+    )
+
+    news = relationship('News', backref='comments')
+    user = relationship('User', backref='comments')
+
+    def __repr__(self):
+        return '<Comment {}>'.format(self.id)
--- webapp/user/views.py
+++ webapp/user/views.py
@@ -4,6 +4,7 @@ from flask_login import current_user, login_user, logout_user
 from webapp.db import db
 from webapp.user.forms import LoginForm, RegistrationForm
 from webapp.user.models import User
+from webapp.utils import get_redirect_target
 
 
 blueprint = Blueprint('user', __name__, url_prefix='/users')
@@ -12,7 +13,7 @@ blueprint = Blueprint('user', __name__, url_prefix='/users')
 @blueprint.route('/login')
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('news.index'))
+        return redirect(get_redirect_target)
     title = 'Authorization'
     login_form = LoginForm()
     return render_template('user/login.html', page_title=title,
@@ -27,7 +28,7 @@ def process_login():
         if user and user.check_password(form.password.data):
             login_user(user, remember=form.remember_me.data)
             flash('Successfull')
-            return redirect(url_for('news.index'))
+            return redirect(get_redirect_target)
     flash('Not correct login or password')
     return redirect(url_for('user.login'))
 
